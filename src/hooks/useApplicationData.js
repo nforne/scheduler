b2d7e5b730e6b9d55fbe871dbc68fca37ce7c6d8
nforne <mnforne@gmail.com>
2021-12-15 08:00:27 -0500

    add free spots update funtionality for days

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,13 +1,49 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
+import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "../helpers/selectors.js";
 
 export default function useApplicationData(initial) {
 
-    const [state, setState] = useState(initial);
+  const [state, setState] = useState(initial);
+  const [changeTracker, setChangeTracker] = useState(0);
 
   const setDay = (day) => setState({ ...state, day });
   // const setDays = (days) => setState(prev => ({ ...prev, days }));
   // const setAppointments = (appointments) => setState(prev => ({ ...prev, appointments }));
+//--------------------------------------------------------------------------------------------------x 
+//   const dailyAppointments = getAppointmentsForDay(state, state.day);
+
+//   const spotsLeft = (state, dailyAppointments) => {
+    
+//         let selectedDay = [{}, 'i'];
+//         for (let j = 0; j < state.days.length; j++) {
+//             if (state.days[j].name === state.day) {
+//                 selectedDay = [state.days[j], j];
+//                 break;
+//                 } 
+//             }
+//         let onpenSpots = 0
+//         for (const appointment of dailyAppointments) {
+//             if (appointment.interview === null) {
+//                 onpenSpots += 1;
+//             }
+//         }
+
+//         const newDays = (state, day) => {
+//             const days = [...state.days];
+//             if (days[selectedDay[1]].id === day.id) {
+//                 days[selectedDay[1]] = day;
+//             }
+//             return days;
+//         }
+        
+//         const day = {...selectedDay[0], spots: onpenSpots}
+//         const days = newDays(state, day)
+
+//         return [days, day, onpenSpots];
+//     };
+
+//   const updates = () => spotsLeft(state, dailyAppointments);
   
   const bookInterview = (id, interview, vcgfn, vObj) => { // vcgfn = view change function, vObj = view Object
     const appointment = {
@@ -18,8 +54,13 @@ export default function useApplicationData(initial) {
       ...state.appointments,
       [id]: appointment
     };
+
     axios.put(`/api/appointments/${id}`, {'interview':interview})
-        .then(() => {setState(prev => ({...prev, appointments})); vcgfn(vObj.SHOW);})
+        .then(() => {
+            setChangeTracker(prev => prev + 1);
+            setState(prev => ({...prev, appointments /*, days: updates()[0]*/}));
+            vcgfn(vObj.SHOW); 
+            })
         .catch((error) => {vcgfn(vObj.ERROR_SAVE, true); console.log(error)})
    };
   
@@ -32,13 +73,17 @@ export default function useApplicationData(initial) {
       ...state.appointments,
       [id]: appointment
     };
+
     axios.delete(`/api/appointments/${id}`, {'interview': interview })
-        .then(() => {setState(prev => ({...prev, appointments})); vcgfn(vObj.EMPTY); })
+        .then(() => {
+            setChangeTracker(prev => prev + 1);
+            setState(prev => ({...prev, appointments /*, days: updates()[0]*/})); 
+            vcgfn(vObj.EMPTY); 
+           })
         .catch((error) => {vcgfn(vObj.ERROR_DELETE, true); console.log(error)})
    };
 
    useEffect(() => {
-
     Promise.all([
       axios.get('/api/days'),
       axios.get('/api/appointments'),
@@ -49,10 +94,9 @@ export default function useApplicationData(initial) {
         appointments : all[1].data, 
         interviewers: all[2].data
       }))
-
-    });
-         
-  }, []);
+    }); 
+          
+  }, [changeTracker]);
   
  return {setDay, bookInterview, cancelInterview, state };
 };
